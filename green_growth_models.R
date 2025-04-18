b07a0257c51a22f2ab99ad0e80e1df533557d6c7
wgetgoose <carson@carsonferreira.com>
2025-04-17 17:41:19 -0700

    comments

--- green_growth_models.R
+++ green_growth_models.R
@@ -7,7 +7,7 @@ library(readxl)
 # import wb dataset
 sids_sectors <- read_excel("sids_sectors.xlsx")
 
-#computing change in vars
+# add "change_year" columns to data frame
 sids_var_names <- c("y1990", "y1991", "y1992", "y1993", "y1994", "y1995", "y1996", 
                     "y1997", "y1998", "y1999", "y2000", "y2001", "y2002", 
                     "y2003", "y2004", "y2005", "y2006", "y2007", "y2008", "y2009",       
@@ -15,20 +15,23 @@ sids_var_names <- c("y1990", "y1991", "y1992", "y1993", "y1994", "y1995", "y1996
                     "y2018", "y2019", "y2020", "y2021", "y2022", "y2023")
 indices <- c(1:length(sids_var_names))
 output <- sids_sectors
+# 2:length() because data is 1990-2023 but change is 1991-2023 (no change in first year of data)
 for (i in 2:length(sids_var_names)) {  
   output <- output %>%
     mutate(
       !!paste0("change_", sids_var_names[i]) := !!sym(sids_var_names[i]) - !!sym(sids_var_names[i - 1])
     )
 }
-#tidying output
+
+# remove cols with just year, keeping only the "change_yXXXX" cols
 output <- output %>% 
   select(-c("y1990", "y1991", "y1992", "y1993", "y1994", "y1995", "y1996", 
             "y1997", "y1998", "y1999", "y2000", "y2001", "y2002", 
             "y2003", "y2004", "y2005", "y2006", "y2007", "y2008", "y2009",       
             "y2010", "y2011", "y2012", "y2013", "y2014", "y2015", "y2016", "y2017",
             "y2018", "y2019", "y2020", "y2021", "y2022", "y2023"))
-#create year col
+
+# create year col
 net_long <- output %>%
   pivot_longer(
     cols = starts_with("change_y"),
@@ -37,16 +40,19 @@ net_long <- output %>%
     values_to = "value"
   ) %>%
   mutate(year = as.integer(year))
-#breakout series into discrete columns
+
+# breakout series into discrete columns
 net_wide <- net_long %>%
   pivot_wider(
     id_cols = c(`Country Name`, `Country Code`, year),
     names_from = `Series Name`,
     values_from = value
   )
-#saving
+
+# saving
 write_csv(net_wide, "net_sids_tidy.csv")
-#percent sector contribution to growth
+
+# percent sector contribution to growth
 error <- net_wide %>% 
   select(-carbon_intensity) %>% 
   mutate(net_exports = exports-imports) %>% 
@@ -69,4 +75,3 @@ final <- net_wide %>%
 ggplot(data = final, mapping = aes(x = net_exports))+
   geom_histogram()
 write_csv(final, "sids_gm_attempt1.csv")
-  
